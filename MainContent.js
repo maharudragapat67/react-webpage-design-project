d82add2f0ce3d34ee56102762c0b7bf9ee7decff
maharudragapat67 <rudragapat235@gmail.com>
2024-11-30 11:50:52 +0530

    Add files via upload

---
+++ MainContent.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './MainContent.css';
+
+const MainContent = () => {
+    return (
+        <main className="main-content">
+            <section className="intro">
+                <h1>Welcome to My Website</h1>
+                <p>Your solution for modern web design and development.</p>
+                <button>Learn More</button>
+            </section>
+        </main>
+    );
+};
+
+export default MainContent;
